84eda3593053c8aa94a5658bc70208f8499497c5
AsiraIsmail <42769372+AsiraIsmail@users.noreply.github.com>
2018-12-19 14:32:57 +0530

    Create natural.java

---
+++ natural.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class natural
+{
+    public static void main(String args[])
+    {
+        int x, i=1 ;
+        int sum =0 ;
+         Scanner s = new Scanner(System.in);
+        x = s.nextInt();
+        while(i <= x)
+        {
+            sum = sum +i;
+            i++;
+        }
+        System.out.println(sum);
+    } 
+}
