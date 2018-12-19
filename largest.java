3b809526bee211da4c3ad6853809743d954af838
AsiraIsmail <42769372+AsiraIsmail@users.noreply.github.com>
2018-12-19 12:09:29 +0530

    Update largest.java

--- largest.java
+++ largest.java
@@ -1,25 +1,25 @@
 import java.util.Scanner;
- 
-class Main
+public class largest
 {
-  public static void main (String[] args) 
-  {
-    int x, y, z;
-    System.out.println("Enter three integers");
-    Scanner in = new Scanner(System.in);
+    public static void main(String[] args) 
+    {
+        int x, y, z;
+        Scanner s = new Scanner(System.in);
+        x = s.nextInt();
+        y = s.nextInt();
+        z = s.nextInt();
+        if(x > y && x > z)
+        {
+            System.out.println(x);
+        }
+        else if(y > z)
+        {
+            System.out.println(y);
+        }
+        else
+        {
+            System.out.println(z);
+        }
  
-    x = in.nextInt();
-    y = in.nextInt();
-    z = in.nextInt();
- 
-    if (x > y && x > z)
-      System.out.println("First number is largest.");
-    else if (y > x && y > z)
-      System.out.println("Second number is largest.");
-    else if (z > x && z > y)
-      System.out.println("Third number is largest.");
-    else
-      System.out.println("The numbers are not distinct.");
-  }
+    }
 }
-
