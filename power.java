0d934b5cd7eb6be6e36cfa68a07dafec250b3318
AsiraIsmail <42769372+AsiraIsmail@users.noreply.github.com>
2018-12-19 15:17:50 +0530

    Create power.java

---
+++ power.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+class power
+{
+   public static void main(String args[]){
+      Scanner sc = new Scanner(System.in);
+      int base = sc.nextInt();
+      int temp = base;
+      
+      int exp = sc.nextInt();
+
+      for (int i=1; i<exp; i++){
+         temp = temp*temp;
+      }
+      System.out.println(temp);
+   }
+}
